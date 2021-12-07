040f627574e8b30171556f641f6a516ddf15c00f
Huynh <46445099+nguhcv@users.noreply.github.com>
2021-12-07 19:56:29 +0900

    Update model.py

--- n3ml/model.py
+++ n3ml/model.py
@@ -818,7 +818,58 @@ class Diehl2015(nn.Module):
             threshold[2] = max(threshold[2], x.max())
         return x
 
-    
+ 
+class SNN_Huynh2021(nn.Module):
+    def __init__(self, batch_size: int):
+        super(SNN_Huynh2021, self).__init__()
+        self.batch_size = batch_size
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=12, kernel_size=(5, 5), stride=(1, 1), padding=0, bias=False)
+        self.conv1_if = SoftIF2D_channel_thresholds(batch_size=batch_size,num_channels=12,height=24, width=24,reset=0.0)
+        self.pool1 = nn.AvgPool2d(2)
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels=64, kernel_size=(5, 5), stride=(1, 1), padding=0,
+                               bias=False)
+        self.conv2_if = SoftIF2D_channel_thresholds(batch_size=batch_size, num_channels=64, height=8, width=8,reset=0.0)
+        self.pool2 = nn.AvgPool2d(2)
+        self.fc1 = nn.Linear(in_features=1024, out_features=10, bias=False)
+        self.fc1_if = IF1d_NoSpike(batch_size=batch_size,num_features=10)
+
+        self.active = nn.ReLU()
+        self.flat = nn.Flatten()
+
+
+    def init_neuron_models(self):
+        for m in self.named_children():
+            if isinstance(m[1], IF1d_NoSpike) or isinstance(m[1], SoftIF2D_channel_thresholds):
+                m[1].init_vars()
+
+    def update_threshold(self, threshold: List[float]) -> None:
+        self.conv1_if.update_threshold(threshold[0])
+        self.conv2_if.update_threshold(threshold[1])
+        # self.fc1_if.threshold = threshold[2]
+
+    def forward(self, images: torch.Tensor, inference = False, ):
+
+        x= images
+        if inference:
+            rand_list = torch.rand(images.size()).cuda()
+            x = torch.zeros(images.size()).cuda()
+            x.masked_fill_(rand_list < images, 1.)
+
+        x = self.conv1(x)
+        x = self.conv1_if(x)
+        # output = self.drop(output)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.conv2_if(x)
+        # output = self.drop(output)
+        x = self.pool2(x)
+        x = self.flat(x)
+        x = self.fc1(x)
+        x = self.fc1_if(x)
+        return x
+
+
 class SNN_Diehl2015(nn.Module):
     def __init__(self, batch_size: int, threshold: List[float] = None):
         super(SNN_Diehl2015,  self).__init__()
